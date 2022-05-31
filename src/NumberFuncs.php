38bc9588c48e76fa29c7967dec24def90526ccfe
Erlend Eliassen <gloatingcord26@github.com>
2022-05-31 13:09:14 +0200

    Feat: Constructor

--- src/NumberFuncs.php
+++ src/NumberFuncs.php
@@ -4,27 +4,29 @@ namespace GloatingCord26\BasicCalculator;
 
 class NumberFuncs implements NumberInterface
 {
-    public function __construct(private float|int $num1, private float|int $num2)
-    {
+    public function __construct(
+        private float|int $num1,
+        private float|int $num2
+    ) {
     }
 
-    public function add(int|float $num1, int|float $num2): int|float
+    public function add(): int|float
     {
-        return $this->{$num1} + $this->{$num2};
+        return $this->num1 + $this->num2;
     }
 
-    public function sub(int|float $num1, int|float $num2): int|float
+    public function sub(): int|float
     {
-        return $this->{$num1} - $this->{$num2};
+        return $this->num1 - $this->num2;
     }
 
-    public function mul(int|float $num1, int|float $num2): int|float
+    public function mul(): int|float
     {
-        return $this->{$num1} * $this->{$num2};
+        return $this->num1 * $this->num2;
     }
 
-    public function dev(int|float $num1, int|float $num2): int|float
+    public function dev(): int|float
     {
-        return $this->{$num1} / $this->{$num2};
+        return $this->num1 / $this->num2;
     }
 }
